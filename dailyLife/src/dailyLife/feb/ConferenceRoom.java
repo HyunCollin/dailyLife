37902a6e1e1bae50f9154cb2e3cf7dab98e0dd7b
Hyun <sunghyunpd@gmail.com>
2017-02-08 15:18:48 +0900

    // 2 2 2 2 2 = 2 // 3 1 1 1 2 1 3 = 2 // 6 1 3 3 100 4 5 6 6 5 6 7 7 = 5 // 11 1 4 3 5 0 6 5 7 3 8 5 9 6 10 8 11 8 12 2 13 12 14 = 4 // 10 8 8 8 8 8 8 8 8 6 8 8 8 8 8 8 8 8 8 8 8 = 10 풀이 중 사용한 입력 값 예 문제의 요구사항을 잘못 이해한점과 틀린 입력값 찾기가 어려웠다.

--- dailyLife/src/dailyLife/feb/ConferenceRoom.java
+++ dailyLife/src/dailyLife/feb/ConferenceRoom.java
@@ -5,57 +5,62 @@ import java.util.Scanner;
 
 public class ConferenceRoom {
 	private static int conferenceNum = 0;
+	private static int startRoomTime = 0;
+	private static int[][] roomTime = null;
+	private static int[] endCon = null;
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		conferenceNum = scanner.nextInt();
-		
-		int[][] roomTime = new int[conferenceNum][2];
-		
+
+		roomTime = new int[conferenceNum][2];
+		endCon = new int[conferenceNum];
+
 		for (int i = 0; i < conferenceNum; i++) {
 			roomTime[i][0] = scanner.nextInt();
 			roomTime[i][1] = scanner.nextInt();
+			endCon[i] = 0;
 		}
-				
-		sortOrderAsc(roomTime);
-		
-		printEndTimeSeq(roomTime);
-		
+
+		int total = 0;
+		while (true) {
+			int nowConference = getConferenceNo();
+
+			if (nowConference == -1) {
+				System.out.println(total);
+				break;
+			}
+
+			total++;
+			startRoomTime = roomTime[nowConference][1];
+			endCon[nowConference] = 1;
+			endCon[nowConference] = 1;
+		}
+
 		scanner.close();
 	}
 
-	private static void printEndTimeSeq(int[][] roomTime) {
-		int fStartTime = 0;
-		int totalCnt = 0;
+	private static int getConferenceNo() {
+		int temp = 2147483647;
+		int tI = -1;
 		for (int i = 0; i < conferenceNum; i++) {
-			if(roomTime[i][0] >= fStartTime){
-//				System.out.println(roomTime[i][0] + ", " + roomTime[i][1]);
-				totalCnt++;
-				fStartTime = roomTime[i][1];
+			if (temp >= roomTime[i][1] && endCon[i] == 0 && startRoomTime <= roomTime[i][0]) {
+				int k = i;
+				if(tI >= 0 ){
+					if(roomTime[tI][0] < roomTime[k][0] && roomTime[tI][1] == roomTime[k][1]){
+						k = tI;
+					}
+				}				
+				temp = roomTime[k][1];
+				tI = k;				
 			}
 		}
-		System.out.println(totalCnt);
-	}
-	
-	private static void sortOrderAsc(int[][] maps) {
-		for (int i = 0; i < conferenceNum; i++) {
-			int compare = maps[i][1];
-			int tIndex = 0;
-			
-			for (int j = i; j < conferenceNum; j++) {
-				if(compare >= maps[j][1]){
-					compare = maps[j][1];
-					tIndex = j;
-				}
-			}
-			if(tIndex > 0 ){
-				int tempA = maps[i][0];
-				int tempB = maps[i][1];
-				maps[i][0] = maps[tIndex][0];
-				maps[i][1] = maps[tIndex][1];
-				maps[tIndex][0] = tempA;
-				maps[tIndex][1] = tempB;
-			}
-		}		
+		return tI;
 	}
 }
+// 2 2 2 2 2 = 2
+// 3 1 1 1 2 1 3 = 2
+// 6 1 3 3 100 4 5 6 6 5 6 7 7 = 5
+// 11 1 4 3 5 0 6 5 7 3 8 5 9 6 10 8 11 8 12 2 13 12 14 = 4
+// 10 8 8 8 8 8 8 8 8 6 8 8 8 8 8 8 8 8 8 8 8 = 10
